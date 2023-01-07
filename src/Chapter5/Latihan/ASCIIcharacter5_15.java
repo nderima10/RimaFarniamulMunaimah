32c1e9a3eccebccac8aba463d0b92c0e8c5593b3
nderima10 <nderima.02@gmail.com>
2023-01-07 15:51:51 +0700

    first

---
+++ src/Chapter5/Latihan/ASCIIcharacter5_15.java
@@ -0,0 +1,17 @@
+package Chapter5.Latihan;
+
+public class ASCIIcharacter5_15 {
+    public static void main(String[] args) {
+        final  int NUMBER_OF_CHARACTER_PER_LINE = 10;
+        int count = 0;
+
+        for (int i = 33; i <= 126; i++) {
+            count++;
+            if (count % 10 == 0)
+                System.out.println((char) i);
+            else
+                System.out.print((char) i + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/Chapter5/Latihan/PiramidLoop5_19.java
@@ -0,0 +1,22 @@
+package Chapter5.Latihan;
+
+public class PiramidLoop5_19 {
+    public static void main(String[] args) {
+        int mulai = 0, akhir = 7;
+
+        for (int baris = 1; baris <= 128; baris += baris) {
+            for (int ruang = 0; ruang < akhir; ruang++) {
+                System.out.print("    ");
+            }
+            for (int I = 1; I <= baris; I += I) {
+                System.out.printf("%4d", (I));
+            }
+            for (int b = mulai; b > 0; b /= 2) {
+                System.out.printf("%4d", (b));
+            }
+            System.out.println();
+            akhir--;
+            mulai = baris;
+        }
+    }
+}
