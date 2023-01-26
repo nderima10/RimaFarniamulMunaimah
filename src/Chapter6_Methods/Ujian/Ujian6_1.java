153b4f0128aae23b1cd965b01b062d0f882f8f0c
nderima10 <nderima.02@gmail.com>
2023-01-26 16:25:15 +0700

    Ujian chapter_6

---
+++ src/Chapter6_Methods/Ujian/Ujian6_1.java
@@ -0,0 +1,24 @@
+package Chapter6_Methods.Ujian;
+
+public class Ujian6_1 {
+    public static void main(String[] args) {
+        System.out.println(
+                """
+
+                        Inch     Meters     |      Meters      Inch
+                        ------------------------------------------------""");
+        for (double inch = 1.0, meters = 1.0; inch <= 100.0; inch++, meters += 1) {
+            System.out.printf("%4.1f    ", inch);
+            System.out.printf("%6.4f", inchToMeters(inch));
+            System.out.print("      |     ");
+            System.out.printf("%4.1f     ", meters);
+            System.out.printf("%7.4f\n", metersToInch(meters));
+        }
+    }
+    public static double inchToMeters (double inch) {
+        return 0.0254 * inch;
+    }
+    public static double metersToInch (double meter) {
+        return 39.3701 * meter;
+    }
+}
