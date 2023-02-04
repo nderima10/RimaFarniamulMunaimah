35a14d6a43ca8298e38117560b0a882b0e3fe091
nderima10 <nderima.02@gmail.com>
2023-02-04 17:02:21 +0700

    Ujian chapter_6

--- src/Chapter6_Methods/Ujian/Ujian6_3.java
+++ src/Chapter6_Methods/Ujian/Ujian6_3.java
@@ -4,8 +4,128 @@ import java.util.Scanner;
 
 public class Ujian6_3 {
     public static void main(String[] args) {
+
+        String masuk = null, keluar = null, hasilAkhir = null, nomor = null;
+        int menu = MENU();
+
+        switch (menu) {
+            case 1 : {
+                masuk = "Biner";
+                keluar = "Oktal";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 2 : {
+                masuk = "Biner";
+                keluar = "Desimal";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 3 : {
+                masuk = "Biner";
+                keluar = "Heksa";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 4 : {
+                masuk = "Oktal";
+                keluar = "Biner";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 5 : {
+                masuk = "Oktal";
+                keluar = "Desimal";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 6 : {
+                masuk = "Oktal";
+                keluar = "Heksa";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 7 : {
+                masuk = "Desimal";
+                keluar = "Biner";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 8 : {
+                masuk = "Desimal";
+                keluar = "Oktal";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 9 : {
+                masuk = "Desimal";
+                keluar = "Heksa";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+            }
+            case 10 : {
+                masuk = "Heksa";
+                keluar = "Biner";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 11 : {
+                masuk = "Heksa";
+                keluar = "Oktal";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+                break;
+            }
+            case 12 : {
+                masuk = "Biner";
+                keluar = "Desimal";
+
+                nomor = inputNomor(masuk,keluar);
+                boolean hasil = cekInput(nomor,masuk);
+                konversiNomor (hasil,nomor,masuk,keluar);
+            }
+        }
+        assert false;
+        if (hasilAkhir.equals("Error")) {
+            System.out.println("Bilangan yang anda masukan salah!");
+        }else {
+            System.out.println(nomor.toLowerCase() + " dalam " + masuk + " adalah " + hasilAkhir.toLowerCase() + " dalam " + keluar);
+        }
+    }
+    public static int MENU() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Mengkonversi bilangan Biner ");
+        System.out.println(" Mengkonversi bilangan ");
         System.out.println("=============== Menu ===============\n");
         System.out.println("1. Konversi bilangan biner ke oktal");
         System.out.println("2. Konversi bilangan biner ke desimal");
@@ -19,25 +139,200 @@ public class Ujian6_3 {
         System.out.println("10. Konversi bilangan heksa ke biner");
         System.out.println("11. Konversi bilangan heksa ke oktal");
         System.out.println("12. Konversi bilangan heksa ke desimal");
-
-        System.out.print("Masukan nomor MENU : ");
-        int MENU = input.nextInt();
+        System.out.println(" ");
+        System.out.print("Masukan menu yang akan dikonversi : ");
+        return input.nextInt();
     }
 
-    public static double konversiHtoD(int hex) {
-        if (hex != 1) {
-            System.out.println();
-            System.exit(1);
-        }
-        char ch1 = (char) hex;
-        if (ch1 <= 'F' && ch1 >= 'A') {
-            int v = ch1 - 'A' + 10;
-            System.out.println("Konversi bilangan desimalnya" + ch1 + " adalah " + v);
-        } else if (Character.isDigit(ch1)) {
-            System.out.println("Konversi bilangan desimalnya " + ch1 + " adalah " + ch1);
+    private static String inputNomor(String masuk, String keluar) {
+        Scanner input = new Scanner(System.in);
+        String  nomor;
+        System.out.println(masuk + " ke " + keluar);
+        System.out.print("Masukan " + masuk + " Nomor : ");
+        nomor = input.next();
+        return nomor;
+    }
+
+    private static String konversiNomor(boolean hasil, String nomor, String masuk, String keluar) {
+        String hasilAkhir;
+        if (hasil) {
+            if (masuk.equals("Biner") && keluar.equals("Oktal")) {
+                hasilAkhir = (binerToOktal(nomor));
+            } else if (masuk.equals("Biner") && keluar.equals("Desimal")) {
+                hasilAkhir = String.valueOf(binerToDesimal(nomor));
+            } else if (masuk.equals("Biner") && keluar.equals("Heksa")) {
+                hasilAkhir = (binerToHeksa(nomor));
+            } else if (masuk.equals("Oktal") && keluar.equals("Biner")) {
+                hasilAkhir = (oktalToBiner(nomor));
+            } else if (masuk.equals("Oktal") && keluar.equals("Desimal")) {
+                hasilAkhir = (oktalToDesimal(nomor));
+            } else if (masuk.equals("Oktal") && keluar.equals("Heksa")) {
+                hasilAkhir = (oktalToHeksa(nomor));
+            } else if (masuk.equals("Desimal") && keluar.equals("Biner")) {
+                hasilAkhir = (desimalToBiner(nomor));
+            } else if (masuk.equals("Desimal") && keluar.equals("Oktal")) {
+                hasilAkhir = (desimalToOktal(nomor));
+            } else if (masuk.equals("Desimal") && keluar.equals("Heksa")) {
+                hasilAkhir = (desimalToHeksa(nomor));
+            } else if (masuk.equals("Heksa") && keluar.equals("Biner")) {
+                hasilAkhir = String.valueOf(heksaToBiner(nomor));
+            } else if (masuk.equals("Heksa") && keluar.equals("Oktal")) {
+                hasilAkhir = (heksaToOktal(nomor));
+            } else if (masuk.equals("Heksa") && keluar.equals("Desimal")) {
+                hasilAkhir = (heksaToDesimal(nomor));
+            } else {
+                hasilAkhir = "Error";
+            }
         } else {
-            System.out.println("Bilangan hexa tidak tersedia");
+            hasilAkhir = "Error";
         }
-        return ch1;
+        return hasilAkhir;
+    }
+    private static boolean cekInput(String nomor, String cek) {
+        switch (cek){
+            case "Biner" -> {
+                return nomor.matches("[01]+");
+            }
+            case "Desimal" -> {
+                return nomor.matches("-?");
+            }
+            case "Heksadesimal" -> {
+                return nomor.matches("^[0-9a-fA-F]+$");
+            }
+            case "Oktal" -> {
+                return nomor.matches("^[0-7]+$");
+            }
+            default -> throw new IllegalArgumentException("Unexpected value: " + cek);
+        }
+    }
+    public static String binerToOktal(String nomor) {
+        int desimal = Integer.parseInt(String.valueOf(nomor), 2);
+        return Integer.toOctalString(desimal);
+    }
+    public static int binerToDesimal(String nomor) {
+        return Integer.parseInt(String.valueOf(nomor), 2);
+    }
+    public static String binerToHeksa(String nomor) {
+        int desimal = Integer.parseInt(String.valueOf(nomor), 2);
+        return Integer.toHexString(desimal);
+    }
+    public static String oktalToBiner(String nomor) {
+        StringBuilder biner = new StringBuilder();
+        for (int i = 0; i < nomor.length(); i++) {
+            int digit = nomor.charAt(i) - '0';
+            StringBuilder bin = new StringBuilder();
+            for (int y = 0; y < 3; y++) {
+                bin.insert(0, (digit & 1));
+                digit = digit >> 1;
+            }
+            biner.append(bin);
+        }
+        return biner.toString();
+    }
+    public static String oktalToDesimal(String nomor) {
+        StringBuilder desimal = new StringBuilder();
+        for (int i = 0; i < nomor.length(); i++) {
+            char c = nomor.charAt(i);
+            int digit = c - '0';
+            desimal.append(digit * Math.pow(8, nomor.length() - i - 1));
+        }return desimal.toString();
+    }
+    public static String oktalToHeksa(String nomor) {
+        int desimal = 0;
+        for (int i = 0; i < nomor.length(); i++) {
+            char c = nomor.charAt(i);
+            int digit = c - '0';
+            desimal += digit * Math.pow(8, nomor.length() - i - 1);
+        }
+        StringBuilder heksa = new StringBuilder();
+        char[] heksaChar = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        while (desimal > 0) {
+            long t = desimal % 16;
+            heksa.insert(0, heksaChar[(int) t]);
+            desimal = desimal / 16;
+        }
+        return heksa.toString();
+    }
+    public static String desimalToBiner(String nomor) {
+        int desimal = Integer.parseInt(nomor);
+        StringBuilder biner = new StringBuilder();
+        while (desimal > 0 ) {
+            long a = desimal % 2;
+            biner.insert(0, a);
+            desimal = desimal / 2;
+        }
+        return biner.toString();
+    }
+    public static String desimalToOktal(String nomor) {
+        int desimal = Integer.parseInt(nomor);
+        return Integer.toOctalString(desimal);
+    }
+    public static String desimalToHeksa(String nomor) {
+        int desimal = Integer.parseInt(nomor);
+        return Integer.toHexString(desimal);
+    }
+    public static String heksaToBiner(String nomor) {
+        nomor = nomor.toUpperCase();
+        StringBuilder biner = new StringBuilder();
+        for (int i  = 0; i < nomor.length(); i++) {
+            char c = nomor.charAt(i);
+            switch (c) {
+                case '0' : biner.append("0000");
+                case '2' : biner.append("0001");
+                case '3' : biner.append("0010");
+                case '4' : biner.append("0100");
+                case '5' : biner.append("0101");
+                case '6' : biner.append("0110");
+                case '7' : biner.append("0111");
+                case '8' : biner.append("1000");
+                case '9' : biner.append("1001");
+                case 'A' : biner.append("1010");
+                case 'B' : biner.append("1011");
+                case 'C' : biner.append("1100");
+                case 'D' : biner.append("1101");
+                case 'E' : biner.append("1110");
+                case 'F' : biner.append("1111");
+            }
+        }
+        return String.valueOf(biner);
+    }
+    public static String heksaToOktal(String nomor) {
+        int desimal = 0;
+        nomor = nomor.toUpperCase();
+        for (int i = 0; i < nomor.length(); i++){
+            char c = nomor.charAt(i);
+            int digit;
+            if (c >= '0' && c <= '9') {
+                digit = c - '0';
+            } else if (c >= 'A' && c <= 'F') {
+                digit = c - 'A' + 10;
+            } else {
+                throw new IllegalArgumentException("Invalid heksadesimal digit : " + c);
+            }
+            desimal += digit * Math.pow(16, nomor.length() - i - 1);
+        }
+        StringBuilder oktal = new StringBuilder();
+        while (desimal > 0) {
+            oktal.insert(0, (desimal & 7));
+            desimal = desimal >> 3;
+        }
+        return oktal.toString();
+    }
+    public static String heksaToDesimal(String nomor) {
+        nomor = nomor.toUpperCase();
+        int desimal = 0;
+        for (int i = 0; i < nomor.length(); i++) {
+            char c = nomor.charAt(i);
+            int digit;
+            if (c >= '0' && c <= '9') {
+                digit = c - '0';
+            } else if (c >= 'A' && c <= 'F') {
+                digit = c - 'A' + 10;
+            } else {
+                throw new IllegalArgumentException("invalid heksa digit : " + c);
+            }
+            desimal += digit * Math.pow(16, nomor.length() - i - 1);
+        }
+        return String.valueOf(desimal);
     }
 }
--- src/Chapter6_Methods/Ujian/Ujian6_4.java
+++ src/Chapter6_Methods/Ujian/Ujian6_4.java
@@ -1,108 +1,107 @@
 package Chapter6_Methods.Ujian;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Ujian6_4 {
+    static SimpleDateFormat thn = new SimpleDateFormat("mmmm");
+    static SimpleDateFormat bul = new SimpleDateFormat("yyyy");
+    static Date tanggal = new Date();
+    static String bulan = bul.format(tanggal);
+    static String tahun = thn.format(tanggal);
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.print("Slip Gaji karyawan Bulan Februari ");
-        int tahun = input.nextInt();
-        System.out.print("");
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println(" ");
         System.out.print("Nama karyawan        : ");
         String nama = input.next();
         System.out.print("Status               : ");
-        double status = input.nextDouble();
-        switch ((int) status){
-            case 1:
-                System.out.println("                       menikah");
-                break;
-            case 2:
-                System.out.println("                       Belum Menikah");
-                break;
-            case 3:
-                System.out.println("                       Duda");
-                break;
-            case 4:
-                System.out.println("                       Janda");
-        }
+        String status = input.next();
         System.out.print("Jumlah anak          : ");
-        double anak = input.nextDouble();
+        int anak = input.nextInt();
         System.out.print("Jarak rumah - kantor : ");
-        double jarak = input.nextDouble();
+        int jarak = input.nextInt();
         System.out.print("Jumlah masuk kerja   : ");
-        double jumlah = input.nextDouble();
+        int jumlah = input.nextInt();
         System.out.print("Tahun masuk          : ");
-        double tahunMasuk = input.nextDouble();
+        int tahunMasuk = input.nextInt();
         System.out.print("Gaji pokok           : ");
-        double gajiPokok = input.nextDouble();
+        int gajiPokok = input.nextInt();
         System.out.println(" ");
 
-        System.out.println(tunjangan(anak,status,jarak,jumlah,tahunMasuk,tahun,gajiPokok));
+        int tunjanganKeluarga = tKeluarga(gajiPokok, status, anak);
+        int tunjanganTransfortasi = tTransfortasi(jumlah, jarak);
+        int tunjanganKesehatan = tKesehatan(tahunMasuk);
 
+        int totalGaji = gajiPokok + tunjanganKeluarga + tunjanganTransfortasi + tunjanganKesehatan;
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Slip Gaji karyawan Bulan " + bulan + " " + tahun);
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println(" ");
+        System.out.println("Nama karyawan        : " + nama);
+        System.out.println("Status               : " + status);
+        System.out.println("Jumlah anak          : " + anak);
+        System.out.println("Jarak rumah - kantor : " + jarak);
+        System.out.println("Jumlah masuk kerja   : " + jumlah);
+        System.out.println("Tahun masuk          : " + tahunMasuk);
+        System.out.println(" ");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("\tGajih Pokok\t|\tT.Keluarga\t|\tT.Transport\t|\tT.Kesehatan\t|\t");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.printf("\t%,d\t|\t\t%,d\t\t|\t%,d\t\t|\t%,d\t\t|\n", gajiPokok, tunjanganKeluarga,tunjanganTransfortasi,tunjanganKesehatan);
+        System.out.println(" ");
+        System.out.println("Total Gaji Bulan " + bulan + " " + totalGaji);
     }
-    public static int tunjangan(double anak, double status, double jarak, double jumlah, double tahunMasuk,double tahun, double gajiPokok) {
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println(
-                """
-                        Gaji pokok      |     T. Keluarga     |   T. Transportasi   |     T. Kesehatan    \s
-                        --------------------------------------------------------------------------------""");
-        System.out.printf("%4.0f", gajiPokok);
-        System.out.print("        |      ");
+    private static int tKeluarga (int gajiPokok, String status, int anak) {
+        double tKel = 0;
+        status = status.toLowerCase();
 
-        double tKeluarga = 0;
-        if ( status == 2)
-            System.out.printf("%4.0f", tKeluarga = gajiPokok * 0.0);
-        else if (status == 1 && anak == 0)
-            System.out.printf("%4.0f", tKeluarga = gajiPokok * 0.05);
-        else if (anak <= 3)
-            System.out.printf("%4.0f", tKeluarga = gajiPokok * 0.075);
-        else if (anak >= 3)
-            System.out.printf("%4.0f", tKeluarga = gajiPokok * 0.010);
-        else if (status == 3 && anak > 0)
-            System.out.printf("%4.0f", tKeluarga = gajiPokok * 0.05);
-        else if (status == 4 && anak > 0)
-            System.out.printf("%4.0f",tKeluarga = gajiPokok * 0.075);
-        else if (status == 3)
-            System.out.printf("%4.0f",tKeluarga = gajiPokok *0.03);
-        else if (status == 4)
-            System.out.printf("%4.0f",tKeluarga = gajiPokok * 0.05);
-        else
-            System.out.printf("%4.0f", "Error");
-        System.out.print("         |      ");
-
-        // tunjangan transfortasi
-        double tTransfortasi = 0;
-        if (jarak <= 5) {
-            System.out.printf("%4.0f",tTransfortasi = 5000 * jumlah);
-        } else if (jarak >= 5 & jarak <= 10) {
-            System.out.printf("%4.0f",tTransfortasi = 10000 * jumlah);
-        } else if (jarak >= 10) {
-            System.out.printf("%4.0f",tTransfortasi = 15000 * jumlah);
-        }
-        System.out.print("         |      ");
-
-        //tunjangan kesehatan
-        double tKesehatan;
-        tahunMasuk = tahun - tahunMasuk;
-        double bulan = 0;
-        if (tahunMasuk <= 2) {
-            System.out.printf("%8.0f",tKesehatan = 200000 + bulan);
-        } else if (tahunMasuk <= 5) {
-            System.out.printf("%8.0f",tKesehatan = 500000 + bulan);
+        if ("Menikah".equals(status) && anak == 0) {
+            tKel = gajiPokok * 0.05;
+        } else if ("Menikah".equals(status) && anak <= 3) {
+            tKel = gajiPokok * 0.075;
+        } else if ("Menikah".equals(status)) {
+            tKel = gajiPokok * 0.010;
+        } else if ("Duda" .equals(status) && anak > 0) {
+            tKel = gajiPokok * 0.05;
+        } else if ("Janda".equals(status) && anak > 0) {
+            tKel = gajiPokok * 0.075;
+        } else if ("Duda".equals(status) && anak == 0) {
+            tKel = gajiPokok * 0.03;
+        } else if ("Janda".equals(status) && anak == 0) {
+            tKel = gajiPokok * 0.05;
         } else {
-            System.out.printf("%8.0f",tKesehatan = 750000 + bulan);
+            System.out.print("Error");
         }
-        System.out.println();
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println();
-        double total = gajiPokok + tKeluarga + tTransfortasi + tKesehatan;
-        System.out.printf("Total Gaji Bulan Februari : %4.0f\n", total);
-        System.out.println("---------------------------------------------------------------------------------");
+        return (int) tKel;
+    }
+    private static int tTransfortasi (int jumlah, int jarak) {
+        double tTrans;
 
-    return 0;
+        if (5 >= jarak) {
+            tTrans = 5000 * jumlah;
+        } else if (jarak <= 10) {
+            tTrans = 10000 * jumlah;
+        } else {
+            tTrans = 15000 * jumlah;
+        }
+        return (int) tTrans;
+    }
+    private static int tKesehatan(int tahunMasuk) {
+        int tahunIni = Integer.parseInt(tahun);
+        int tKes = 0;
+        int lama = tahunIni - tahunMasuk;
+        if (lama < 2) {
+            tKes = 200000;
+        } else if (lama < 5) {
+            tKes = 500000;
+        } else if (lama > 5) {
+            tKes = 750000;
+        } else {
+            System.out.println("Error");
+        }
+        return tKes;
     }
 }
